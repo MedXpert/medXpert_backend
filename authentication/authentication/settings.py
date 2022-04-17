71405f478a500acc5aff5ef626c6ee3c24993403
davenbezz <it.dawit.bezabih@gmail.com>
2022-04-17 23:29:04 +0300

    A core app added

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Apps
+    'rest_framework',
+
+    # Local Apps
+    'core',
 ]
 
 MIDDLEWARE = [
