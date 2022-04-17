6a1eb0c6027565fb9d7cf1e457f8722f514edb3c
davenbezz <it.dawit.bezabih@gmail.com>
2022-04-18 00:29:44 +0300

    Accounts app added for login and signup purpose

--- authentication/core/urls.py
+++ authentication/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.HelloView.as_view(), name='hello'),
+        path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
