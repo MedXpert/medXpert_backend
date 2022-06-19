483dcba3aa20eea93dc3d004b99e5e537d9fa183
Michael Belete <it.michael.belete@gmail.com>
2022-06-19 16:30:33 +0300

    admin change

--- api/admin.py
+++ api/admin.py
@@ -2,21 +2,36 @@ from django.contrib import admin
 import phonenumbers
 
 from .models import ClaimRequest, HealthCareFacility, User
+
+
 class HealthCareFacilityAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'phoneNumbers', 'email')
     list_filter = ('name', 'address', 'phoneNumbers', 'email')
     search_fields = ('name', 'address', 'phoneNumbers', 'email')
 
+
 class ClaimRequestAdmin(admin.ModelAdmin):
-    list_display = ('requesterFirstName', 'requesterLastName', 'requesterEmail', 'requesterPhoneNumber', 'isDone')
-    list_filter = ('requesterFirstName', 'requesterLastName', 'requesterEmail', 'requesterPhoneNumber', 'isDone')
-    search_fields = ('requesterFirstName', 'requesterLastName', 'requesterEmail', 'requesterPhoneNumber', 'isDone')   
+    list_display = ('requesterFirstName', 'requesterLastName',
+                    'requesterEmail', 'requesterPhoneNumber', 'isDone')
+    list_filter = ('requesterFirstName', 'requesterLastName',
+                   'requesterEmail', 'requesterPhoneNumber', 'isDone')
+    search_fields = ('requesterFirstName', 'requesterLastName',
+                     'requesterEmail', 'requesterPhoneNumber', 'isDone')
+
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'firstName', 'lastName', 'sex', 'role', 'phoneNumber')
-    list_filter = ('email', 'firstName', 'lastName', 'sex', 'role', 'phoneNumber')
-    search_fields = ('email', 'firstName', 'lastName', 'sex', 'role', 'phoneNumber')
+    list_display = ('email', 'firstName', 'lastName',
+                    'sex', 'role', 'phoneNumber')
+    list_filter = ('email', 'firstName', 'lastName',
+                   'sex', 'role', 'phoneNumber')
+    search_fields = ('email', 'firstName', 'lastName',
+                     'sex', 'role', 'phoneNumber')
 
+    exclude = ('user_permissions', 'groups', 'isDeleted', 'CreatedDate', 'isActive', 'Last login')
+
+admin.site.site_header = "MedXPert Admin"
+admin.site.site_title = "MedXpert Admin Portal"
+admin.site.index_title = "Welcome to MedXpert Portal"
 
 admin.site.register(User, UserAdmin)
 admin.site.register(ClaimRequest, ClaimRequestAdmin)
